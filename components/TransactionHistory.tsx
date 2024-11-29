adfc46909f1e4d4e0ded2376c156a4dab70ab878
NiftyCoffee <zoetayss@gmail.com>
2024-11-29 15:34:18 +1100

    Implemented pull-to-refresh feature

--- components/TransactionHistory.tsx
+++ components/TransactionHistory.tsx
@@ -1,11 +1,27 @@
-import { FlatList, Text, StyleSheet, View } from "react-native";
+import { FlatList, Text, StyleSheet, View, RefreshControl } from "react-native";
 import { Transaction } from "../models/transactions";
 import TransactionView from "./TransactionView";
+import { useCallback, useState } from "react";
 
 const TransactionHistory = ({ transactions }: { transactions: Transaction[] }) => {
+    const [refreshing, setRefreshing] = useState(false);
+
+    /**
+     * Handles on refresh with timeout of 1000 ms.
+     */
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        setTimeout(() => {
+          setRefreshing(false);
+        }, 1000);
+    }, []);
+
     return transactions.length > 0 ? (
         <FlatList 
         style={styles.container}
+        refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
         data={transactions}
         renderItem={({item}) => <TransactionView transaction={item} />}
             />
