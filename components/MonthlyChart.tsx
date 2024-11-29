42df6edb0e8de2dee0f2ca4fd7779c7460d9fa44
NiftyCoffee <zoetayss@gmail.com>
2024-11-29 17:11:12 +1100

    Implemented Chart display with Victory Native

--- components/MonthlyChart.tsx
+++ components/MonthlyChart.tsx
@@ -1,5 +1,6 @@
-import { StyleSheet, View, Text } from "react-native"
+import { View } from "react-native"
 import { Transaction } from "../models/transactions"
+import { VictoryAxis, VictoryBar, VictoryChart, VictoryLabel, VictoryTheme } from "victory-native";
 
 const MonthlyChart = ({ transactions }: { transactions: Transaction[] }) => {
 
@@ -13,18 +14,44 @@ const MonthlyChart = ({ transactions }: { transactions: Transaction[] }) => {
     .filter(transaction => transaction.type === "debit")
     .reduce((total, transaction) => total + transaction.amount, 0);
 
+    // Format data for chart
+    const data = [
+        {
+            name: "Income",
+            value: income
+        },
+        {
+            name: "Expenses",
+            value: expenses
+        }
+    ]
+
     return (
-        <View style={styles.container}>
-            <Text>Income: {income}</Text>
-            <Text>Expenses: {expenses}</Text>
+        <View>
+            <VictoryChart 
+                theme={VictoryTheme.material}
+                domainPadding={85} 
+                domain={{ y: [0, Math.max(income, expenses) * .4] }}
+                height={200}>
+                <VictoryAxis 
+                    tickValues={["Income", "Expenses"]} 
+                    tickFormat={["Income", "Expenses"]}
+                    style={{ 
+                        tickLabels: { fontSize: 16 },
+                        axis: { stroke: "transparent" }
+                    }} />
+                <VictoryBar
+                    data={data}
+                    x="name"
+                    y="value"
+                    style={{ data: { fill: "#9575ff" } }}
+                    barWidth={60}
+                    labels={({ datum }) => datum.value}
+                    labelComponent={<VictoryLabel style={{ fontSize: 16 }} />}
+                />
+            </VictoryChart>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        // flex: 1,
-    }
-})
 
 export default MonthlyChart;
